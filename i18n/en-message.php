3379c540c1c5a8a19322607b3f77d79fc6db482c
Johnny Tumlinson <staticgeek@gmail.com>
2013-07-30 10:06:25 -0500

    Create en-message.php

---
+++ i18n/en-message.php
@@ -0,0 +1,24 @@
+<?php
+
+/** 
+* Language file for plugin static_content
+* Language: EN - English
+*
+* @package Plugins
+* @subpackage static_content
+*
+* @author 
+    /**
+* Language file for plugin funky_cache
+*
+* @package Plugins
+* @subpackage funky_cache
+*
+* @author Leedtech | http://leedtech.com
+* @version Wolf 0.7.7
+**/
+
+return array(
+  //index.php
+
+  );
