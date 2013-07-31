eb6a0bc32b5bdb2c123c86041e710916bad44c4f
Johnny Tumlinson <staticgeek@gmail.com>
2013-07-31 09:11:44 -0500

    Update index.php
    Add core functionality to script

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php
-
 /*
-* Static Content for Wolf CMS. <http://www.wolfcms.org>
-*
-* Copyright (C) 2013 Leedtech <http://leedtech.com>
-* Copyright (c) 2008-2009 Mika Tuupola
-*
-* This file is part of the plugin Static Content for Wolf CMS. It is licensed
-* under the GPL v3 license.
-*
-* For details, see: http://fsf.org/
-*/
+ * Static Content for Wolf CMS.
+ * 
+ * Copyright (C) 2013 Leedtech <http://www.leedtech.com>
+ *
+ * This file is part of the plugin Static Content for Wolf CMS.  It is
+ * licensed under the GPLv3 license.
+ * 
+ * The license can be viewed at http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
 
 /* Security Feature */
 if (!defined('IN_CMS')) {
@@ -23,12 +21,27 @@ Plugin::setInfos(array(
   'description' => 'Enables delivery of static content from another web server.',
   'version' => '0.1',
   'license' => 'GPL v3',
-  'author' => 'Leedtech <http://leedtech.com>',
+  'author' => 'Leedtech <http://www.leedtech.com>',
   'require_wolf_version' => '0.7.7+',
   //'update_url' => '',
   'website' => 'http://leedtech.com'
   ));
 
+$url_file = PLUGINS_ROOT.'/static_content/content.static';             // Static URL file location
+
+if(file_exists($url_file)) {
+  $static_url = file_get_contents('content.static', true);             // Read file contents
+  $static_url = explode(';;',$static_url);                             // Explode file contents
+
+  define('URL_STATIC', 'http://'.$static_url[0].'/');                  // Define URL_STATIC
+  define('STHEMES_URI', 'http://'.$static_url[1].'/');                 // Define STHEMES_URI
+  }
+
+else {
+  define('URL_STATIC', URL_PUBLIC.'public/');                          //File doesn't exist, URL_STATIC = public directory
+  define('STHEMES_URI', THEMES_URI);                                    // File doesn't exist, STHEMES_URI = THEMES_URI
+  }
+  
 /* Stuff for backend. */
 if (defined('CMS_BACKEND')) {
 
